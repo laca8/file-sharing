8062dfcd8fb0ce0471b3a26cd4a615a50bba54b8
laca8 <ahmedragab01140331005@gmail.com>
2025-02-23 18:51:39 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
 const cors = require("cors");
 const globalError = require("./middlewares/errorhandler");
 dotenv.config();
